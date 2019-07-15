cca2446bc09f85d0a421af159c1b6115560cd57c
Max Adulyanukosol <namax7@gmail.com>
2019-07-16 00:56:53 +0700

    Add SectionPage for openers

---
+++ web/components/SectionPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="page">
+    <div class="bg" :style="{'background-image': `url(${bgImg})`, 'background-position': bgPos}">
+      <div :style="{'flex-grow': spacers[0]}"></div>
+      <div class="wordbox">
+        <h2 v-html="sectionString"></h2>
+      </div>
+      <div :style="{'flex-grow': spacers[1]}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sectionString: {
+      type: String,
+      default: '(section)'
+    },
+    spacers: {
+      type: Array,
+      default: () => [5, 1]
+    },
+    bgImg: {
+      default: require('~/assets/img/bg/cristian-newman-CeZypKDceQc-unsplash.jpg')
+    },
+    bgPos: {
+      type: String,
+      default: 'center center'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.page {
+  @apply w-full h-full bg-purple-400;
+}
+
+.bg {
+  @apply h-full max-w-3xl m-auto flex flex-col items-center justify-center;
+  background-size: cover;
+}
+
+.wordbox {
+  @apply rounded-lg shadow-md m-5 p-10 text-gray-100 text-2xl font-semibold;
+  /* @apply bg-purple-800 opacity-75; */
+  background-color: hsla(256, 44%, 42%, 0.85);
+}
+
+@screen md {
+  .wordbox {
+    @apply text-3xl;
+  }
+}
+</style>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -3,24 +3,39 @@
 <template>
   <div class="container">
     <full-page ref="fullpage" :options="options" id="fullpage">
-      <div class="section"><TitlePage :titleString="$t('title')" /></div>
-      <div class="section">つづく</div>
+      <div class="section">
+        <TitlePage :titleString="$t('title')" />
+      </div>
+      <div class="section">
+        <SectionPage :sectionString="$t('section-1')" />
+      </div>
+      <div class="section">
+        <SectionPage
+          :sectionString="$t('section-2')"
+          :spacers="[1, 3]"
+          :bgImg="require('~/assets/img/bg/online-marketing-hIgeoQjS_iE-unsplash.jpg')"
+          bgPos="right center"
+        />
+      </div>
+      <div class="section bg-purple-200">つづく</div>
     </full-page>
   </div>
 </template>
 
 <script>
 import TitlePage from '~/components/TitlePage.vue'
+import SectionPage from '~/components/SectionPage.vue'
 
 export default {
   components: {
     TitlePage,
+    SectionPage
   },
   data() {
     return {
       options: {
         licenseKey: 'IN_REQUEST',
-        anchors: ['home', 'tbc'],
+        anchors: ['home', 'section-1-0', 'section-2-0', 'tbc'],
         verticalCentered: true,
         controlArrows: true,
         scrollBar: true,
